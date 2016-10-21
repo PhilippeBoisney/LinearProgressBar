c75324cae28d4058c8776843cc888d85dbb69aed
Cody Winton <cody@credntia.com>
2016-10-21 14:09:24 -0500

    View Controller updates

--- LinearProgressBar/ViewController.swift
+++ LinearProgressBar/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let linearBar: LinearProgressBar = LinearProgressBar()
+	var linearBar: LinearProgressBar = {
+		
+		var bar = LinearProgressBar(frame: CGRect(x: 10, y: UIScreen.main.bounds.height - 200, width: UIScreen.main.bounds.width - 20, height: 0))
+		
+		bar.backgroundColor = UIColor(red:0.68, green:0.81, blue:0.72, alpha:1.0)
+		bar.progressBarColor = UIColor(red:0.26, green:0.65, blue:0.45, alpha:1.0)
+		bar.heightForLinearBar = 5
+		bar.widthRatioOffset = 0.78
+		bar.xOffset = 15
+		bar.keyframeDuration = 1.2
+		
+		return bar
+	}()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.configureLinearProgressBar()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,18 +38,6 @@ class ViewController: UIViewController {
     
     @IBAction func stopAnimation(_ sender: AnyObject) {
         self.linearBar.dismiss(duration: 0.3)
-    }
-    
-    //-----
-    
-    fileprivate func configureLinearProgressBar() {
-        linearBar.backgroundColor = UIColor(red:0.68, green:0.81, blue:0.72, alpha:1.0)
-        linearBar.progressBarColor = UIColor(red:0.26, green:0.65, blue:0.45, alpha:1.0)
-        linearBar.heightForLinearBar = 5
-		linearBar.widthRatioOffset = 0.78
-		linearBar.xOffset = 15
-		
-		linearBar.keyframeDuration = 1.2
     }
 }
 
