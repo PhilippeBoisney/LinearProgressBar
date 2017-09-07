5f2413beeef00899f78b65e4c7ed0e5b223756d8
Eric Marchand <eric.marchand@4d.com>
2017-09-07 17:11:00 +0200

    Add framework target in project and share it (carthage compatibility)

---
+++ LinearProgressBarFramework/LinearProgressBarFramework.h
@@ -0,0 +1,19 @@
+//
+//  LinearProgressBarFramework.h
+//  LinearProgressBarFramework
+//
+//  Created by Eric Marchand on 07/09/2017.
+//  Copyright © 2017 CookMinute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LinearProgressBarFramework.
+FOUNDATION_EXPORT double LinearProgressBarFrameworkVersionNumber;
+
+//! Project version string for LinearProgressBarFramework.
+FOUNDATION_EXPORT const unsigned char LinearProgressBarFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinearProgressBarFramework/PublicHeader.h>
+
+
